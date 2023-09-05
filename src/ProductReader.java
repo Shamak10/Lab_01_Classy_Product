1f1b8f882ea70f289e944494f74a3d8a06ef8e3c
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-09-05 11:28:48 -0400

    Update ProductReader.java

--- src/ProductReader.java
+++ src/ProductReader.java
@@ -1,65 +1,59 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.*;
 import java.util.ArrayList;
-import java.util.Scanner;
-// FOR WRITING PRODUCT
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class ProductReader {
     public static void main(String[] args) {
-        // Initialize an ArrayList to store product data
-        ArrayList<String> products = new ArrayList<>();
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        ArrayList<Product> products = new ArrayList<>();
 
-        // Create a Scanner object to read user input
-        Scanner scanner = new Scanner(System.in);
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
 
-        // Prompt the user to enter product data in a specific format
-        System.out.println("Enter product data using the format: ID, Name, Description, Cost");
-        System.out.println("Example: 000001, Pipeweed, Long Bottom Leaf, 600.0");
-        System.out.println("Enter 'exit' to finish.");
-
-        while (true) {
-            String productData = "";
-
-            // Get ID
-            String id = SafeInput.getNonZeroLenString(scanner, "Enter ID");
-            productData += id + ", ";
-
-            // Get Name
-            String name = SafeInput.getNonZeroLenString(scanner, "Enter Name");
-            productData += name + ", ";
-
-            // Get Description
-            String description = SafeInput.getNonZeroLenString(scanner, "Enter Description");
-            productData += description + ", ";
-
-            // Get Cost
-            double cost = SafeInput.getDouble(scanner, "Enter Cost");
-            productData += cost;
-
-            // Add the formatted product data to the ArrayList
-            products.add(productData);
-
-            // Check if the user wants to add another product
-            if (!SafeInput.getYNConfirm(scanner, "Do you want to add another product?")) {
-                break;
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                readProductsFromFile(file, products);
+                displayProducts(products);
             }
-        }
-
-        // Get a filename from the user with a valid format using regular expression
-        String filename = SafeInput.getRegExString(scanner, "Enter a filename to save the data", "[a-zA-Z0-9]+\\.txt");
-
-        // Write product data to the specified file
-        try (FileWriter writer = new FileWriter(filename)) {
-            // Loop through the list of products and write each product's data to the file
-            for (String product : products) {
-                writer.write(product + System.lineSeparator());
-            }
-            System.out.println("Data saved to " + filename);
         } catch (IOException e) {
-            // Handle any errors that might occur during file writing
-            System.out.println("Error writing to file.");
-        } finally {
-            // Close the scanner to prevent resource leak
-            scanner.close();
+            System.err.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void readProductsFromFile(Path file, ArrayList<Product> products) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            int line = 0;
+            String lineData;
+            while ((lineData = reader.readLine()) != null) {
+                String[] rec = lineData.split(", ");
+                if (rec.length == 4) {
+                    String code = rec[0];
+                    String name = rec[1];
+                    String description = rec[2];
+                    double price = Double.parseDouble(rec[3]);
+
+                    Product product = new Product(code, name, description, price);
+                    products.add(product);
+                    line++;
+                    System.out.printf("Line %4d: %s%n", line, product);
+                } else {
+                    System.out.printf("Line %4d: Invalid data format: %s%n", line, lineData);
+                }
+            }
+            System.out.println("\nData file read successfully!");
+        }
+    }
+
+    private static void displayProducts(ArrayList<Product> products) {
+        for (int i = 0; i < products.size(); i++) {
+            System.out.printf("Product %d: %s%n", i + 1, products.get(i));
         }
     }
 }
