1e23326f952b970dcc5e1a9efe0f52781d8923d5
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-09-05 10:58:12 -0400

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Product {
+public class Product {
+    private String name;
+    private String description;
+    private String ID;
+    private double cost;
+
+    public Product(String ID, String name, String description, double cost) {
+        this.ID = ID;
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String toCSVDataRecord() {
+        return ID + "," + name + "," + description + "," + cost;
+    }
 }
